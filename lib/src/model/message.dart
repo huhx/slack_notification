42c3b23ca673214dbb354022133b436d1895e677
hongxiang <gohuhx@163.com>
2023-03-14 16:57:24 +0800

    [huhx] -feat: add Attachment

--- lib/src/model/message.dart
+++ lib/src/model/message.dart
@@ -1,16 +1,25 @@
+import 'attachment.dart';
 import 'block.dart';
 
 class Message {
   final String text;
   final List<Block>? blocks;
+  final List<Attachment>? attachments;
 
-  const Message({required this.text, this.blocks});
+  const Message({
+    required this.text,
+    this.blocks,
+    this.attachments,
+  });
 
   Map<String, dynamic> toJson() {
     return {
       'text': text,
       if (blocks != null && blocks!.isNotEmpty)
-        'blocks': blocks!.map((e) => e.toJson()).toList(),
+        'blocks': blocks!.map((block) => block.toJson()).toList(),
+      if (attachments != null && attachments!.isNotEmpty)
+        'attachments':
+            attachments!.map((attachment) => attachment.toJson()).toList(),
     };
   }
 }
